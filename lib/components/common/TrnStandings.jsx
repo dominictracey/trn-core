cebc585f05648a141b38d75e385fecde394ea637
Scott <Skotbb@gmail.com>
2017-01-26 15:39:38 -0500

    Added standings to the Competition Category page.

---
+++ lib/components/common/TrnStandings.jsx
@@ -0,0 +1,111 @@
+import React, {PropTypes, Component} from 'react'
+import { withRouter } from 'react-router'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { getActions, Components, registerComponent, getRawComponent, withList } from 'meteor/nova:core'
+import Categories from 'meteor/nova:categories'
+import gql from 'graphql-tag';
+
+
+class TrnStandings extends Component {
+
+	constructor() {
+		super();
+		//this.getStandings = this.getStandings.bind(this);
+
+		// always define an inital state!
+		this.state = {
+			trnId: -1,
+			loadingComp: false,
+			showStandings: false
+		};
+	}
+
+	componentWillReceiveProps(nextProps) {
+		if (this.props.params.slug != nextProps.params.slug) {
+			this.props.params.slug = nextProps.params.slug
+			this.getStandings(this.props.results)
+		}
+	}
+
+	async componentDidMount() {
+		const {results: categories = []} = this.props
+		await this.getStandings(categories)
+	}
+
+	async getStandings(categories) {
+		const {loadCompStandings, loadCompetition} = this.props
+		var trnId;
+
+		const results = categories ? categories.filter(cat => this.props.params.slug == cat.slug) : null
+		if (results != null && results.length > 0) {
+			trnId = results[0].trnId
+		}
+		console.log("Loading Standings")
+		await loadCompStandings(trnId)
+		await loadCompetition(trnId)
+		console.log("Load complete")
+		this.setState({trnId: trnId, showStandings: true})
+	}
+
+	render() {
+		const {compStandingsById, teams} = this.props
+
+
+		var trnId = this.state.trnId != -1 ? this.state.trnId : null
+		const standingsArr = trnId && compStandingsById && teams && compStandingsById[trnId].standings ? compStandingsById[trnId].standings : null
+		var retval = null;
+		if(this.state.showStandings && standingsArr){
+			retval = -1
+		}
+		else if(this.state.showStandings && !standingsArr){
+			retval = <div>No Standings for this Competition</div>
+		}
+		else{
+			retval = <Components.Loading />
+		}
+
+		return (
+			<div className='standings-container'>
+				<div className='standings-head'>Standings</div>
+				{
+					retval && retval == -1 ? standingsArr.map((standing, index) => {
+						return  (
+							<div className='standings-results'>
+								<strong>{standing.standing}</strong>
+								{teams[standing.teamId].displayName}
+							</div>
+						)
+					}): retval
+				}
+			</div>
+		)
+	}
+}
+
+TrnStandings.fragment = gql`
+  fragment standingFragment on Category {
+    _id    
+    slug
+    trnId    
+  }
+`
+
+const options = {
+	collection: Categories,
+	queryName: 'categoriesSingleQuery',
+	fragment: TrnStandings.fragment,
+	limit: 0,
+};
+
+// TrnStandings.propTypes = {
+//   // post: React.PropTypes.object.isRequired
+// };
+
+TrnStandings.displayName = "TrnStandings"
+
+const mapStateToProps = ({entities: {teams, compStandingsById}}) => ({teams, compStandingsById})
+const {loadCompStandings, loadCompetition} = getActions();
+const mapDispatchToProps = dispatch => bindActionCreators({loadCompStandings, loadCompetition}, dispatch);
+
+registerComponent('TrnStandings', TrnStandings, withRouter, withList(options), connect(mapStateToProps, mapDispatchToProps))
