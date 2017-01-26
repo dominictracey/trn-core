72c93d6f2c74cc7160f52e14368cb1c2424e42d3
Dominic Tracey <dominic.tracey@rugby.net>
2017-01-26 15:40:32 -0500

    merge stash

--- lib/components/posts/TrnPostsCommentsThread.jsx
+++ lib/components/posts/TrnPostsCommentsThread.jsx
@@ -7,31 +7,31 @@ import gql from 'graphql-tag';
 const TrnPostsCommentsThread = (props, context) => {
 
   const {loading, terms: { postId }, results, totalCount} = props;
-  
+
   if (loading) {
-  
+
     return <div className="posts-comments-thread"><Components.Loading/></div>
-  
+
   } else {
-    
+
     const resultsClone = _.map(results, _.clone); // we don't want to modify the objects we got from props
     const nestedComments = Utils.unflatten(resultsClone, '_id', 'parentCommentId');
-    
+
     return (
       <div className="posts-comments-thread">
         {!!props.currentUser ?
           <div className="posts-comments-thread-new">
             <h4><FormattedMessage id="comments.new"/></h4>
             <Components.CommentsNewForm
-            postId={postId} 
-            type="comment" 
+            postId={postId}
+            type="comment"
             />
           </div> :
           <div>
             <ModalTrigger size="small" component={<a><FormattedMessage id="comments.please_log_in"/></a>}>
               <Components.UsersAccountForm/>
             </ModalTrigger>
-          </div> 
+          </div>
         }
         <Components.CommentsList comments={nestedComments} commentCount={totalCount}/>
       </div>
