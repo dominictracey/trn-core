d5cb416a839491b5fe7cdc53edd1bb14c5e33abb
Scott <Skotbb@gmail.com>
2017-02-03 13:41:36 -0500

    Standings use document instead of list, now.

--- lib/components/common/TrnSidebar.jsx
+++ lib/components/common/TrnSidebar.jsx
@@ -3,11 +3,10 @@ import { withRouter } from 'react-router'
 import { Components, registerComponent } from 'meteor/nova:core';
 
 const TrnSidebar = (context) => {
-  //
+
   const currentCategorySlug = context.params.categoryType && context.params.categoryType == "c" ? context.params.categoryType : null;
-  // const newQuery = _.clone(router.location.query);
-  // newQuery.cat = category.slug;
-  const standings = currentCategorySlug ? <Components.TrnStandings context={context}/> : null
+
+  const standings = currentCategorySlug ? <Components.TrnStandings context={context} slug={context.params.slug} /> : null
 
   return (
     <div className='sidebar-container'>
