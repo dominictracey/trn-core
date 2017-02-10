3c9875c4826888bcf4ec073e84e0bbeb8ca9b8ed
Scott <Skotbb@gmail.com>
2017-02-10 17:23:27 -0500

    FnR working, without new changes.

--- lib/components/common/TrnSbFixturesAndResults.jsx
+++ lib/components/common/TrnSbFixturesAndResults.jsx
@@ -13,118 +13,125 @@ class TrnSbFixturesAndResults extends Component {
 		this.resultsForComp = this.resultsForComp.bind(this);
 
 		this.state = {
-			loading: false,
-			trnId: -1,
-			config: null,
-			currentUro: -1,
 			dispUro: 0
 		}
 	}
 
-	componentDidMount() {
-		const {loadConfiguration, config} = this.props
-
-		//this.state = {loading: true, config: null, trnId: -1}
-		// await loadConfiguration()
-		
-		// use this.setState
-		const newState = Object.keys(config) > 0
-			? {loading: false,
-				config: config,
-				trnId: -1
-			}
-			: {loading: false,
-				trnId: -1,
-				config: null,
-				currentUro: -1,
-			};
-			
-		this.setState(newState);
-	}
+	// componentDidMount() {
+	// 	const { config} = this.props
+	// }
 
 	async componentWillReceiveProps(next) {
 		try {
-			const {universalRoundFnR, document: category, slug, catType, config, loadFixturesAndResults} = this.props
-			var thisConfId
-			
-			// use deepEqual comparison, you are trying to compare huge objects
-			
-			// if (next.config && next.config != this.state.config) {
-			// 	thisConfId = Object.getOwnPropertyNames(next.config).length > 0 ? Object.getOwnPropertyNames(next.config)[0] : null
-			// 	Object.getOwnPropertyNames(next.config).length > 0
-			// 	? this.setState({currentUro: next.config[thisConfId].currentUROrdinal, dispUro: next.config[thisConfId].currentUROrdinal})
-			// 	: null
-			// }
-			
-			const cat = category || next.category ? category || next.category : null
-			cat && this.state.trnId == -1 ? this.setState({trnId: cat.trnId}) : null
-			
-			const callApi = catType == "c" && cat && thisConfId && config.hasOwnProperty(thisConfId) && !universalRoundFnR && this.state.currentUro !=-1 ? await loadFixturesAndResults(this.state.currentUro, category.trnId) : null
+			const {loadConfiguration, universalRoundFnR, document: category, slug, catType, config, loadFixturesAndResults} = this.props
+
+			if(Object.keys(config).length == 0) {
+				loadConfiguration()
+				return
+			}
+			if(this.state.dispUro == 0){
+				this.setState({dispUro: config[Object.keys(config)[0]].currentUROrdinal})
+			}
+
+			let fNRKey = config[Object.keys(config)[0]].currentUROrdinal + category.trnId
+			if(!universalRoundFnR || !universalRoundFnR[fNRKey] ) {
+				loadFixturesAndResults(this.state.dispUro, category.trnId)
+			}
 			
 		} catch(e) {
 			console.error('will receive props error', e);
 		}
 	}
 
-	resultsForComp(rawResults) {
-		//const rawResults = raw.universalRoundFnR
-
-		// Object.keys(rawResults).map((compIdUro) => {
-		// 	return (rawResults[compIdUro].compFandRs.map((comp) => {
-		// 		return <Component.TrnSingleFnR comp={comp} key={comp.compId} />
-		// 	}))
-		// })
+	resultsForComp(uro, catId, rawResults) {
+		const key = uro+catId
 		
-		if(Object.keys(rawResults) > 0){
-			for ( const compIdUro in rawResults){
-				if(rawResults.hasOwnProperty(compIdUro)) {
-					if(rawResults[compIdUro].universalRoundOrdinal == this.state.dispUro) {
-					return (rawResults[compIdUro].compFandRs.map((comp, index) => {
-							return <Components.TrnSingleFnR comp={comp} key={comp.compId}/>
-						})
-					)
-				}
-				}
+		if(rawResults && Object.keys(rawResults).length > 0){
+			if(rawResults[key] && rawResults[key].compFandRs) {
+				return (rawResults[key].compFandRs.map((comp, index) => {
+					return <Components.TrnSingleFnR comp={comp} key={comp.compId}/>
+					})
+				)
+			}
+			else{
+				return <div className="FnR-matchData">No results for this week.</div>
 			}
 		}
 		else {
-			return null;
+			return null
 		}
 	}
 
-	async changeUro(uro){
-		// try {
-		// 	const { document: category, loadFixturesAndResults } = this.props
-		// 	
-		// 	if (uro != -1 && category) {
-		// 		await loadFixturesAndResults(uro, category.trnId)
-		// 		this.setState({dispUro: uro})
-		// 	} 
-		// 	
-		// } catch(e) {
-		// 	console.error('change uro', e);
-		// }
-		console.log('should do something');
+	async changeUro(change, trnId){
+		try {
+			const { loadFixturesAndResults, universalRoundFnR, config } = this.props
+
+			let uro = 0
+			if(Object.keys(config)[0]){
+				uro = this.state.dispUro
+				let cur	= config[Object.keys(config)[0]].currentUROrdinal
+				if(uro+change >= cur-1 && uro+change <= cur+1 ){
+					uro += change
+					this.setState({dispUro: uro})
+				}
+			}
+			let key = uro+trnId
+			if (universalRoundFnR && !universalRoundFnR[key]) {
+				loadFixturesAndResults(uro, trnId)
+			}
+
+		} catch(e) {
+			console.error('change uro', e);
+		}
+		console.log('New fixtures');
 	}
 
 	render() {
-		const {universalRoundFnR} = this.props
-		
-		const fixAndRes = universalRoundFnR ? this.resultsForComp(universalRoundFnR) : null
-		const currentURO = this.state.currentUro != -1 ? this.state.currentUro : null
+		const {universalRoundFnR, config, document: category} = this.props
+
+		let currentURO = Object.keys(config)[0] ? config[Object.keys(config)[0]].currentUROrdinal : null
+		let dispUro = 0
+		let fixAndRes = null
+		let when = null
+
+		if(this.state.dispUro != 0){
+			dispUro = this.state.dispUro
+		}
+		if(universalRoundFnR && category && Object.keys(config)[0]){
+			console.log("I have the Fixtures and Results.")
+			if(dispUro == currentURO){
+				console.log("Display current URO")
+				fixAndRes = this.resultsForComp(config[Object.keys(config)[0]].currentUROrdinal, category.trnId,  universalRoundFnR)
+				when = <div className="sidebar-card-divider">Current</div>
+				console.log(`Ordinal: ${config[Object.keys(config)[0]].currentUROrdinal}, ID: ${category.trnId}, Thing: ${universalRoundFnR[currentURO.toString()+category.trnId.toString()].compFandRs[0].compId}`)
+			}
+			else if(dispUro == currentURO-1){
+				console.log("Display previous URO")
+				fixAndRes = this.resultsForComp(dispUro, category.trnId,  universalRoundFnR)
+				when = <div className="sidebar-card-divider">Last</div>
+				console.log(`Ordinal: ${dispUro}, ID: ${category.trnId}`)
+			}
+			else if(dispUro == currentURO+1){
+				console.log("Display next URO")
+				fixAndRes = this.resultsForComp(dispUro, category.trnId,  universalRoundFnR)
+				when = <div className="sidebar-card-divider">Next</div>
+				console.log(`Ordinal: ${dispUro}, ID: ${category.trnId}`)
+			}
+		}
+
 
 		return (
 			<div className="sidebar-container">
 				<div className="sidebar-card">
 					<span className="FnR-headerWarrows">
-						{currentURO ? <div className='FnR-arrow' onClick={ this.changeUro(currentURO-1)}>&lt;</div> : null}
+						{currentURO && category ? <div className='FnR-arrow' onClick={() => this.changeUro(-1, category.trnId)}>&lt;</div> : null}
 						<div className='sidebar-card-header'>
 							Fixtures and Results
 							<Components.WiresNewButton prefilledProps={{context: "fixtures sidebar"}} />
 						</div>
-						{currentURO ? <div className='FnR-arrow' onClick={ this.changeUro(currentURO+1)}>&gt;</div> : null}
+						{currentURO && category ? <div className='FnR-arrow' onClick={() => this.changeUro(+1, category.trnId)}>&gt;</div> : null}
 					</span>
-
+					{when ? when : null}
 					<div className='sidebar-card-body'>
 						{
 							fixAndRes ? fixAndRes : <Components.Loading/>
