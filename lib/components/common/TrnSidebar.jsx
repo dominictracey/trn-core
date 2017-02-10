55377c3cec69694ce21db861cfa6e23d629a58cd
xavcz <xavier.cazalot@gmail.com>
2017-02-10 15:09:06 +0100

    RSM-203/4/5

--- lib/components/common/TrnSidebar.jsx
+++ lib/components/common/TrnSidebar.jsx
@@ -1,24 +1,39 @@
 import React from 'react';
 import { withRouter } from 'react-router'
-import { Components, registerComponent } from 'meteor/nova:core';
+import { Components, registerComponent, withCurrentUser } from 'meteor/nova:core';
 
 const TrnSidebar = (props) => {
 
-  const currentCategorySlug = props.params.categoryType && props.params.categoryType == "c" ? props.params.categoryType : null;
-  const standings = currentCategorySlug ? <Components.TrnStandings props={props} slug={props.params.slug} /> : null
-  const profile = /*false ? <Components.TrnSbProfile /> :*/ null
-  const fixturesAndResults = props.params ? <Components.TrnSbFixturesAndResults catType={props.params.categoryType} slug={props.params.slug} /> : null
+  const currentCategorySlug = props.params.categoryType && props.params.categoryType === "c" ? props.params.slug : null;
 
   return (
     <div className='sidebar-container'>
-      {profile}
+      {
+        // show profile sidebar is a user is connected
+        props.currentUser
+        ? <Components.TrnSbProfile documentId={props.currentUser._id} />
+        : null
+      }
+      
       <Components.TrnSbWelcome />
-      {fixturesAndResults}
-      {/* {standings} */}
+      
+      {
+        // show the fixtures & results on a competition page
+        currentCategorySlug 
+        ? <Components.TrnSbFixturesAndResults catType={props.params.categoryType} slug={currentCategorySlug} />
+        : null
+      }
+      
+      {
+        // show the fixtures & results on a competition page
+        currentCategorySlug 
+        ? <Components.TrnStandings props={props} slug={currentCategorySlug} /> 
+        : null
+      }
     </div>
   )
 }
 
 TrnSidebar.displayName = "TrnSidebar";
 
-registerComponent('TrnSidebar', TrnSidebar, withRouter);
+registerComponent('TrnSidebar', TrnSidebar, withRouter, withCurrentUser);
